b2d517d9b24055a1adb87aa073538fda42006db4
Rashi Tiwari <117359006+RashiTiwari12@users.noreply.github.com>
2024-08-08 23:12:47 +0530

    login page updated

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,16 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { Box, Typography, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  useMediaQuery,
+  Grid,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -16,6 +25,9 @@ const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
   const fullname = useRef(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   const handleButtonClick = () => {
     const message = checkValidData(
       email.current.value,
@@ -40,7 +52,7 @@ const Login = () => {
             displayName: fullname.current.value,
           })
             .then(() => {
-              const { uid, email, displayName, photoURL } = auth.currentUser;
+              console.log("");
             })
             .catch((error) => {
               // An error occurred
@@ -61,7 +73,6 @@ const Login = () => {
       )
         .then((userCredential) => {
           // Signed in
-
           router.push("/inventory");
           const user = userCredential.user;
         })
@@ -76,40 +87,39 @@ const Login = () => {
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
+
   return (
-    <>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-        sx={{
-          border: "2px solid red",
-          backgroundImage:
-            'url("https://cdn.pixabay.com/photo/2021/02/01/06/48/geometric-5969508_1280.png")',
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backdropFilter: "blur(10px)",
-          position: "relative",
-        }}
-      >
-        <Box
-          height={500}
-          width={500}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          gap={2}
-          p={2}
-          bgcolor="#fff"
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      sx={{
+        backgroundImage:
+          'url("https://cdn.pixabay.com/photo/2021/02/01/06/48/geometric-5969508_1280.png")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backdropFilter: "blur(10px)",
+        position: "relative",
+      }}
+    >
+      <Grid container justifyContent="center">
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={6}
+          lg={4}
           sx={{
+            bgcolor: "#fff",
+            p: 4,
             border: "2px solid grey",
             borderRadius: 2,
+            boxShadow: 3,
           }}
         >
-          <Typography variant="h4" component="h2" gutterBottom>
+          <Typography variant="h4" component="h2" gutterBottom align="center">
             {isSignInForm ? "Sign In" : "Sign Up"}
           </Typography>
           <form onSubmit={(e) => e.preventDefault()} style={{ width: "100%" }}>
@@ -150,21 +160,63 @@ const Login = () => {
               sx={{ mt: 2 }}
               onClick={handleButtonClick}
             >
-              {isSignInForm ? "Sign In" : "Sign Up"}
+              {isMobile
+                ? isSignInForm
+                  ? "Sign In"
+                  : "Sign Up"
+                : isSignInForm
+                ? "Sign In to Your Account"
+                : "Create Your Account"}
             </Button>
-            <Typography
-              variant="body2"
-              sx={{ mt: 2, cursor: "pointer", textDecoration: "underline" }}
-              onClick={toggleSignInForm}
-            >
-              {isSignInForm
-                ? "New User? Sign up now."
-                : "Already registered? Sign In now"}
+            <Typography variant="body2" sx={{ mt: 2 }}>
+              {isSignInForm ? (
+                <>
+                  New User?{" "}
+                  <Button
+                    variant="text"
+                    color="primary"
+                    sx={{
+                      textDecoration: "underline",
+                      cursor: "pointer",
+                      padding: 0,
+                      minWidth: "auto",
+                      "&:hover": {
+                        backgroundColor: "transparent", // To remove background on hover
+                      },
+                    }}
+                    onClick={toggleSignInForm}
+                  >
+                    Sign up now
+                  </Button>
+                  .
+                </>
+              ) : (
+                <>
+                  Already registered?{" "}
+                  <Button
+                    variant="text"
+                    color="primary"
+                    sx={{
+                      textDecoration: "underline",
+                      cursor: "pointer",
+                      padding: 0,
+                      minWidth: "auto",
+                      "&:hover": {
+                        backgroundColor: "transparent", // To remove background on hover
+                      },
+                    }}
+                    onClick={toggleSignInForm}
+                  >
+                    Sign In now
+                  </Button>
+                  .
+                </>
+              )}
             </Typography>
           </form>
-        </Box>
-      </Box>
-    </>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
